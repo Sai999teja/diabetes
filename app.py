df4f92051eddbf0e949307efbe3211725fa0da88
TELAGAMSETTI SAI TEJA <48620462+Sai999teja@users.noreply.github.com>
2021-03-15 11:57:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,request, url_for, redirect, render_template
 import pickle
 import numpy as np
 import joblib
-import sklearn
+from sklearn.ensemble import GradientBoostingClassifier
 
 app = Flask(__name__)
 
